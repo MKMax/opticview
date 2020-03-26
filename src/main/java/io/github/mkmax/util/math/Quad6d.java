bbd909bba8f3986e9cbf2813fe1fc6808ca8d13d
MKMax <maxim.kasyanenko@protonmail.ch>
2020-03-26 10:11:25 -0400

    save progress

--- src/main/java/io/github/mkmax/util/math/Quad6d.java
+++ src/main/java/io/github/mkmax/util/math/Quad6d.java
@@ -11,8 +11,6 @@ public class Quad6d {
     private final Vector3d qb; // lower-right
     private final Vector3d qc; // upper-right
 
-    private final Matrix3d tb = new Matrix3d (); // transform buffer
-
     public Quad6d (
         final Vector3dc pQa,
         final Vector3dc pQb,
@@ -66,17 +64,62 @@ public class Quad6d {
     }
 
     public Matrix3d map (Quad6d to) {
+        return map (to, new Matrix3d ());
+    }
+
+    public Matrix3d map (Quad6d to, Matrix3d dest) {
+        // this.qa = A
+        // this.qb = B
+        // this.qc = C
+
         // to.qa = D
         // to.qb = E
         // to.qc = F
 
-        tb.setColumn (0, qa);
-        tb.setColumn (0, qb);
-        tb.setColumn (0, qc);
+        final double detAxyBxy = qa.x * qb.y - qb.x * qa.y;
+        final double detAxzBxz = qa.x * qb.z - qb.x * qa.z;
+        final double detBxyCxy = qb.x * qc.y - qc.x * qb.y;
+        final double detBxzCxz = qb.x * qc.z - qc.x * qb.z;
 
-        final double detul = Matrix3ext.;
+        /* TUV is analogous to XYZ and are the vectors of the transformation matrix */
+        final Vector3d T = new Vector3d (1d, 0d, 0d);
+        final Vector3d U = new Vector3d (0d, 1d, 0d);
+        final Vector3d V = new Vector3d (0d, 0d, 1d);
 
-        final Vector3d V =
+        final double Tdiv = qa.x;
+        final double Udiv = detAxyBxy;
+        final double Vdiv = detBxzCxz * detAxyBxy - detBxyCxy * detAxzBxz;
+
+        /* The reason we can completely ignore the computation for
+         * some vectors if their "Xdiv" == 0 is because this is actually
+         * a linear equation we've solved and it turns out the if the
+         * coefficient == 0, then we know that it is a "free variable" (or
+         * vector in our case) and we can just use the respective unit
+         * vector.
+         */
+
+        if (!FloatingPoint.strictEq (Vdiv, 0d)) {
+            V.x = (to.qc.x * qb.x - to.qb.x * qc.x) * detAxyBxy - (to.qb.x * qa.x - to.qa.x * qb.x) * detBxyCxy;
+            V.y = (to.qc.y * qb.x - to.qb.y * qc.x) * detAxyBxy - (to.qb.y * qa.x - to.qa.y * qb.x) * detBxyCxy;
+            V.z = (to.qc.z * qb.x - to.qb.z * qc.x) * detAxyBxy - (to.qb.z * qa.x - to.qa.z * qb.x) * detBxyCxy;
+            V.div (Vdiv);
+        }
+
+        if (!FloatingPoint.strictEq (Udiv, 0d)) {
+            U.x = to.qb.x * qa.x - to.qa.x * qb.x - detAxzBxz * V.x;
+            U.y = to.qb.y * qa.x - to.qa.y * qb.x - detAxzBxz * V.y;
+            U.z = to.qb.z * qa.x - to.qa.z * qb.x - detAxzBxz * V.z;
+            U.div (Udiv);
+        }
+
+        if (!FloatingPoint.strictEq (Tdiv, 0d)) {
+            T.x = to.qa.x - U.x * qa.y - V.x - qa.z;
+            T.y = to.qa.y - U.y * qa.y - V.y - qa.z;
+            T.z = to.qa.z - U.z * qa.y - V.z - qa.z;
+            T.div (Tdiv);
+        }
+
+        return dest.set (T, U, V);
     }
 
 }
