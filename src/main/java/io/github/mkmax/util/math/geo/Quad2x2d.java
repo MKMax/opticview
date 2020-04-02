a56b16a93a8c9011306a886d97c806e2c064ea1e
MKMax <maxim.kasyanenko@protonmail.ch>
2020-04-01 20:32:04 -0400

    save progress

--- src/main/java/io/github/mkmax/util/math/geo/Quad2x2d.java
+++ src/main/java/io/github/mkmax/util/math/geo/Quad2x2d.java
@@ -2,9 +2,7 @@ package io.github.mkmax.util.math.geo;
 
 import org.joml.Matrix3x2dc;
 import org.joml.Vector2dc;
-import org.joml.Matrix3x2d;
 import org.joml.Vector2d;
-import java.util.Objects;
 
 public class Quad2x2d implements Quad2d {
 
@@ -44,72 +42,6 @@ public class Quad2x2d implements Quad2d {
         public void setBottomRightY (double ny)
         { /* do not allow modification but do not throw exception. */ }
     };
-
-    /* +--------------+ */
-    /* | INTERPOLATOR | */
-    /* +--------------+ */
-
-    public static final class Interpolator {
-
-        private static void genMapping (
-            Quad2x2d   from,
-            Quad2x2d   to,
-            Matrix3x2d dest)
-        {
-            final double dx = (to.b.x - to.a.x) / (from.b.x - from.a.x);
-            final double Cx = to.a.x - dx * from.a.x;
-
-            final double dy = (to.b.y - to.a.y) / (from.b.y - from.a.y);
-            final double Cy = to.a.y - dy * from.a.y;
-
-            dest.set (
-                dx, 0d,
-                0d, dy,
-                Cx, Cy
-            );
-        }
-
-        private final Quad2x2d from;
-        private final Quad2x2d to;
-
-        private final Matrix3x2d projection = new Matrix3x2d ();
-        private final Matrix3x2d inverse    = new Matrix3x2d ();
-
-        public Interpolator (Quad2x2d pFrom, Quad2x2d pTo) {
-            from = Objects.requireNonNull (pFrom);
-            to   = Objects.requireNonNull (pTo);
-            update ();
-        }
-
-        public Quad2x2d getFrom () {
-            return from;
-        }
-
-        public Quad2x2d getTo () {
-            return to;
-        }
-
-        public void update () {
-            genMapping (from, to, projection);
-            genMapping (to, from, inverse);
-        }
-
-        public Vector2d unmap (Vector2d dest) {
-            return inverse.transformPosition (dest);
-        }
-
-        public Vector2d unmap (Vector2d dest, Vector2d src) {
-            return inverse.transformPosition (dest, src);
-        }
-
-        public Vector2d map (Vector2d src) {
-            return projection.transformPosition (src);
-        }
-
-        public Vector2d map (Vector2d src, Vector2d dest) {
-            return projection.transformPosition (src, dest);
-        }
-    }
 
     /* +--------------------+ */
     /* | CONSTRUCTOR + DATA | */
@@ -279,9 +211,5 @@ public class Quad2x2d implements Quad2d {
         mat.transformPosition (a, dest.a);
         mat.transformPosition (b, dest.b);
         return dest;
-    }
-
-    public Interpolator interpolate (Quad2x2d to) {
-        return new Interpolator (this, to);
     }
 }
--- src/main/java/io/github/mkmax/util/math/geo/Quad3x2d.java
+++ src/main/java/io/github/mkmax/util/math/geo/Quad3x2d.java
@@ -1,12 +1,8 @@
 package io.github.mkmax.util.math.geo;
 
-import static io.github.mkmax.util.math.LinearAlgebraStatics.*;
-
 import org.joml.Matrix3x2dc;
 import org.joml.Vector2dc;
-import org.joml.Matrix3x2d;
 import org.joml.Vector2d;
-import java.util.Objects;
 
 /* A quad using 3 vectors of 2 components each (3x2) */
 public class Quad3x2d implements Quad2d {
@@ -48,98 +44,6 @@ public class Quad3x2d implements Quad2d {
         public void setBottomRightY (double ny)
         { /* do not allow modification but do not throw exception. */ }
     };
-
-    /* +--------------+ */
-    /* | INTERPOLATOR | */
-    /* +--------------+ */
-
-    public static final class Interpolator {
-
-        private static void genMapping (
-            Quad3x2d   from,
-            Quad3x2d   to,
-            Matrix3x2d dest)
-        {
-            final double Ax = from.a.x;
-            final double Ay = from.a.y;
-            final double Bx = from.b.x;
-            final double By = from.b.y;
-            final double Cx = from.c.x;
-            final double Cy = from.c.y;
-
-            final double Dx = to.a.x;
-            final double Dy = to.a.y;
-            final double Ex = to.b.x;
-            final double Ey = to.b.y;
-            final double Fx = to.c.x;
-            final double Fy = to.c.y;
-
-            final double detBA = det (Bx, By, Ax, Ay);
-            final double detCB = det (Cx, Cy, Bx, By);
-            final double detAC = det (Ax, Ay, Cx, Cy);
-
-            final double iQ = 1d / (detBA + detCB + detAC);
-
-            /* T-vector, aka. X vector */
-            final double Tx = iQ * (Fx * (By - Ay) + Dx * (Cy - By) + Ex * (Ay - Cy));
-            final double Ty = iQ * (Fy * (By - Ay) + Dy * (Cy - By) + Ey * (Ay - Cy));
-
-            /* U-vector, aka. Y vector */
-            final double Ux = iQ * (Fx * (Ax - Bx) + Dx * (Bx - Cx) + Ex * (Cx - Ax));
-            final double Uy = iQ * (Fy * (Ax - Bx) + Dy * (Bx - Cx) + Ey * (Cx - Ax));
-
-            /* V-vector, aka. Z vector (or translation vector) */
-            final double Vx = iQ * (Fx * detBA + Dx * detCB + Ex * detAC);
-            final double Vy = iQ * (Fy * detBA + Dy * detCB + Ey * detAC);
-
-            dest.set (
-                Tx, Ty,
-                Ux, Uy,
-                Vx, Vy
-            );
-        }
-
-        private final Quad3x2d from;
-        private final Quad3x2d to;
-
-        private final Matrix3x2d projection = new Matrix3x2d ();
-        private final Matrix3x2d inverse    = new Matrix3x2d ();
-
-        private Interpolator (Quad3x2d pSrc, Quad3x2d pDest) {
-            from = Objects.requireNonNull (pSrc);
-            to   = Objects.requireNonNull (pDest);
-            update ();
-        }
-
-        public Quad3x2d getFrom () {
-            return from;
-        }
-
-        public Quad3x2d getTo () {
-            return to;
-        }
-
-        public void update () {
-            genMapping (from, to, projection);
-            genMapping (to, from, inverse);
-        }
-
-        public Vector2d unmap (Vector2d dest) {
-            return inverse.transformPosition (dest);
-        }
-
-        public Vector2d unmap (Vector2d dest, Vector2d src) {
-            return inverse.transformPosition (dest, src);
-        }
-
-        public Vector2d map (Vector2d src) {
-            return projection.transformPosition (src);
-        }
-
-        public Vector2d map (Vector2d src, Vector2d dest) {
-            return projection.transformPosition (src, dest);
-        }
-    }
 
     /* +--------------------+ */
     /* | CONSTRUCTOR + DATA | */
@@ -293,9 +197,5 @@ public class Quad3x2d implements Quad2d {
         mat.transformPosition (b, dest.b);
         mat.transformPosition (c, dest.c);
         return dest;
-    }
-
-    public Interpolator interpolate (Quad3x2d dest) {
-        return new Interpolator (this, dest);
     }
 }
